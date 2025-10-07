86f000495d42c657f95aed9ccd35b7b2d82c62b8
Hesam Alizadeh <hesam_alizadeh@bcit.ca>
2025-10-06 20:00:57 -0700

    Dynamically import bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap';
 
+// If you have custom global styles, import them as well:
+// import '../styles/style.css';
+
 function sayHello() {
-    
+
 }
 // document.addEventListener('DOMContentLoaded', sayHello);
